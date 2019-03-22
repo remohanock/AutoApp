527f930be21c059403c14a421a50de0a98b886c9
remo.h <remo.hanock@gmail.com>
2019-03-22 17:42:54 +0530

    album art additions and other alignment of code

--- mobile/src/main/java/com/example/autoapp/activity/HomeActivity.java
+++ mobile/src/main/java/com/example/autoapp/activity/HomeActivity.java
@@ -37,7 +37,9 @@ public class HomeActivity extends AppCompatActivity {
         driverArrayList = new ArrayList<>();
 
         // TODO: 22-03-2019 Drive name and photos to be fetched from configuration file later
-        String[] driverPics = {"https://www.lexellweb.com/wp-content/uploads/2018/08/Planet-Uke-Profile-Image-for-Sidebar-Circular.png","https://www.lexellweb.com/wp-content/uploads/2018/08/Planet-Uke-Profile-Image-for-Sidebar-Circular.png","https://www.morpht.com/sites/morpht/files/styles/landscape/public/dalibor-matura_1.jpg?itok=gxCAhwAV"};
+        String[] driverPics = {"https://www.lexellweb.com/wp-content/uploads/2018/08/Planet-Uke-Profile-Image-for-Sidebar-Circular.png",
+                "https://www.lexellweb.com/wp-content/uploads/2018/08/Planet-Uke-Profile-Image-for-Sidebar-Circular.png",
+                "https://www.morpht.com/sites/morpht/files/styles/landscape/public/dalibor-matura_1.jpg?itok=gxCAhwAV"};
         for (int i = 0; i < driverPics.length; i++) {
             Driver driver = new Driver("Driver " + (i + 1), driverPics[i]);
             driverArrayList.add(driver);
