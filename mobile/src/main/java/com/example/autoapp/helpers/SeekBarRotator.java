b1f1bbc5992deecae640d0c46f0acde9a5aaaa9d
remo.h <remo.hanock@gmail.com>
2019-03-29 13:13:59 +0530

    seek bar code formatting

--- mobile/src/main/java/com/example/autoapp/helpers/SeekBarRotator.java
+++ mobile/src/main/java/com/example/autoapp/helpers/SeekBarRotator.java
@@ -16,7 +16,6 @@ package com.example.autoapp.helpers;/*
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 /*
