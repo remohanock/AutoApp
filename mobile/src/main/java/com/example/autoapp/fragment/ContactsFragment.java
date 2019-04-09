028b6213dd50fe534c0641f6bb4721becd4816db
remo.h <remo.hanock@gmail.com>
2019-04-09 12:03:55 +0530

    sorted contacts alphabetically and integrated touch feel to contact cards

--- mobile/src/main/java/com/example/autoapp/fragment/ContactsFragment.java
+++ mobile/src/main/java/com/example/autoapp/fragment/ContactsFragment.java
@@ -27,6 +27,7 @@ import com.example.autoapp.adapters.ContactsAdapter;
 import com.example.autoapp.models.Contacts;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -85,6 +86,7 @@ public class ContactsFragment extends Fragment {
                 Contacts contacts = new Contacts(contactID, name, phoneNumber, "", "", "");
                 contactsArrayList.add(contacts);
             }
+            Collections.sort(contactsArrayList, (o1, o2) -> o1.getContactName().compareTo(o2.getContactName()));
             phones.close();
         }
     }
